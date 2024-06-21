6068e840a5286ce08caef19fdbecbc02377bb4fe
Cdm220 <catalin.morarasu@student.uaic.ro>
2024-06-22 00:18:14 +0300

    Charts but no response for data

--- client/scripts/get_result.js
+++ client/scripts/get_result.js
@@ -1,4 +1,7 @@
 import { fetchData } from "./service_caller.js";
+import { generateBarChart } from "./bar_chart.js";
+import { generateLineChart } from "./line_chart.js";
+import { generatePieChart } from "./pie_chart.js";
 
 function getSelectedMonthsCount() {
     const sliderValue = parseInt(document.getElementById('timePeriod').value);
@@ -19,14 +22,47 @@ function getSelectedCounties() {
     return Array.from(selectedCounties).map(county => county.value);
 }
 
+function extractLabelAndData(response) {
+    const labels = [];
+    const data = [];
+    labels.push('January');
+    labels.push('February');
+    data.push(10);
+    data.push(20);
+    return { labels, data };
+}
+
+let currentChart = null;
 document.getElementById('applyButton').addEventListener('click', async () => {
     const monthsCount = getSelectedMonthsCount();
     const criterion = getSelectedCriterion();
     const counties = getSelectedCounties();
+
+    const selectedType = document.querySelector('input[name="visualizationType"]:checked').value;
+
     if (monthsCount && criterion) {
         try {
             const response = await fetchData(monthsCount, criterion, counties);
             console.log(response);
+            const { labels, data } = extractLabelAndData(response);
+            const canvas = document.getElementById('chartCanvas').getContext('2d');
+            if (currentChart !== null) {
+                currentChart.destroy();
+            }
+
+            switch (selectedType) {
+                case 'pie':
+                    currentChart = generatePieChart(canvas, labels, data);
+                    break;
+                case 'bar':
+                    currentChart = generateBarChart(canvas, labels, data);
+                    break;
+                case 'graph':
+                    currentChart = generateLineChart(canvas, labels, data);
+                    break;
+                default:
+                    console.error('Unknown chart type selected');
+            }
         } catch (error) {
             alert(error);
         }
