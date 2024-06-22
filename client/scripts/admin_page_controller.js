407ca98005e1c49d348ec66d606e40e9aa8dbb6c
Roice32 <alexandru.butnaru32@yahoo.com>
2024-06-22 18:54:06 +0300

    Reports list done (UI)

--- client/scripts/admin_page_controller.js
+++ client/scripts/admin_page_controller.js
@@ -11,15 +11,56 @@ async function showLogin() {
     document.getElementById('adminPanel').style.display = 'none';
 }
 
+async function buildInfoSectionElement(text, value) {
+    const label = document.createElement('label');
+    label.textContent = text;
+    const field = document.createElement('span');
+    field.textContent = value;
+    
+    const infoSectionElement = document.createElement('div');
+    infoSectionElement.classList.add('labelValueDiv');
+    infoSectionElement.appendChild(label);
+    infoSectionElement.appendChild(field);
+    return infoSectionElement;
+}
+
+async function buildInfoSection(report) {
+    return [ await buildInfoSectionElement('Email:', report.email),
+                await buildInfoSectionElement('Tipul problemei:', issueTypes[Number(report.tip)]),
+                await buildInfoSectionElement('Descriere:', report.descriere)
+    ];
+}
+
+async function buildInteractionSection(report) {
+    const interactionDiv = document.createElement('div');
+    interactionDiv.classList.add('interactionDiv');
+    const textArea = document.createElement('textarea');
+    textArea.placeholder = 'Răspunsul dvs. la problema semnalată';
+    const resolve = document.createElement('button');
+    resolve.textContent = 'Marchează ca rezolvat';
+    resolve.classList.add('actionButton');
+    resolve.id = report.id;
+    interactionDiv.appendChild(textArea);
+    interactionDiv.appendChild(resolve);
+    return interactionDiv;
+}
+
+async function buildOpenReport(report) {
+    const openReportElement = document.createElement('div');
+    const infoSectionElements = await buildInfoSection(report);
+    for (const element of infoSectionElements) {
+        openReportElement.appendChild(element);
+    }
+    openReportElement.appendChild(await buildInteractionSection(report));
+    openReportElement.classList.add('openReport');
+    return openReportElement;
+}
+
 async function buildOpenReportsList() {
     const openReports = await fetchOpenReports();
-    console.log(openReports);
     const reportsList = document.getElementById('openReportsSection');
     for (const report of openReports) {
-        const reportElement = document.createElement('li');
-        reportElement.textContent = `${report.email} - ${issueTypes[Number(report.tip)]} - ${report.descriere}`;
-        reportElement.classList.add('openReport');
-        reportsList.appendChild(reportElement);
+        reportsList.appendChild(await buildOpenReport(report));
     }
 }
 
